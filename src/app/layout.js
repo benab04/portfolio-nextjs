fb6fe4c204496a12bb7f598133a61d5296d9dbca
benab04 <benabrahambiju@gmail.com>
2025-05-11 23:41:53 +0530

    Add Google Analytics tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
@@ -16,6 +17,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-HF2QCF3HDT"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-HF2QCF3HDT');
+        `}
+      </Script>
       <body className={`${inter.className} antialiased min-h-screen bg-black text-white flex flex-col`}>
         <Navbar />
         <main className="flex-grow">{children}</main>
